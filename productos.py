e515e6c2c9317b310c611e6c882601c2cb5922fc
benkx <rosben2@gmail.com>
2020-12-04 00:54:54 -0500

    error 24

--- productos.py
+++ productos.py
@@ -1,6 +1,10 @@
 class Productos:
+    
+    def __init__(self, name):
+        self.produc = []
+
     def nombreProducto(self,name,price):
         self.name = name
         self.price = price
     
-  
+ 
