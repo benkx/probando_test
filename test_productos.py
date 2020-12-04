6ec6c058c236fa3a8e077549c2d8e261327a2cb7
benkx <rosben2@gmail.com>
2020-12-04 00:43:48 -0500

    error 23

--- test_productos.py
+++ test_productos.py
@@ -4,10 +4,12 @@ from productos import Productos
 class TestProductos(unittest.TestCase):
 
     def test_nombre(self):
-       
+        
+        self.nombre = Productos("Panela", 9)
+
        #producto = Prodcutos()
        #self.assertEqual producto.nombreProducto("Manzana", 12)
         
         
-        products = Productos("Manzana")
-        self.assertEqual(products, "Manzana")
+        #products = Productos("Manzana")
+        #self.assertEqual(products, "Manzana")
